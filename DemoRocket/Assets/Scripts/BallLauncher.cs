fd945ca374614a0d78037934e325c0374db40eb6
ctvbds <ctvbds@gmail.com>
2018-07-29 20:48:59 +0700

    fx
    xxx

---
+++ DemoRocket/Assets/Scripts/BallLauncher.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallLauncher : MonoBehaviour {
+
+    //public GameObject ball;
+	public Transform target;
+    Vector3 vetocity = Vector3.zero;
+
+	public float h = 25;
+	public float gravity = -18;
+
+	public bool debugPath;
+
+	void Start() {
+		//ball.useGravity = false;
+	}
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Launch ();
+		}
+
+		if (debugPath) {
+			DrawPath ();
+		}
+        print("hello");
+	}
+
+	void Launch() {
+		Physics.gravity = Vector3.up * gravity;
+		//ball.useGravity = true;
+        vetocity = CalculateLaunchData ().initialVelocity;
+	}
+
+	LaunchData CalculateLaunchData() {
+        float displacementY = target.position.y - vetocity.y;
+        Vector3 displacementXZ = new Vector3 (target.position.x - vetocity.x, 0, target.position.z - vetocity.z);
+		float time = Mathf.Sqrt(-2*h/gravity) + Mathf.Sqrt(2*(displacementY - h)/gravity);
+		Vector3 velocityY = Vector3.up * Mathf.Sqrt (-2 * gravity * h);
+		Vector3 velocityXZ = displacementXZ / time;
+
+		return new LaunchData(velocityXZ + velocityY * -Mathf.Sign(gravity), time);
+	}
+
+	void DrawPath() {
+		LaunchData launchData = CalculateLaunchData ();
+        Vector3 previousDrawPoint = vetocity;
+
+		int resolution = 30;
+		for (int i = 1; i <= resolution; i++) {
+			float simulationTime = i / (float)resolution * launchData.timeToTarget;
+			Vector3 displacement = launchData.initialVelocity * simulationTime + Vector3.up *gravity * simulationTime * simulationTime / 2f;
+            Vector3 drawPoint = vetocity + displacement;
+			Debug.DrawLine (previousDrawPoint, drawPoint, Color.green);
+			previousDrawPoint = drawPoint;
+		}
+	}
+
+	struct LaunchData {
+		public readonly Vector3 initialVelocity;
+		public readonly float timeToTarget;
+
+		public LaunchData (Vector3 initialVelocity, float timeToTarget)
+		{
+			this.initialVelocity = initialVelocity;
+			this.timeToTarget = timeToTarget;
+		}
+		
+	}
+}
+	
--- DemoRocket/Assets/Scripts/playerController.cs
+++ DemoRocket/Assets/Scripts/playerController.cs
@@ -10,13 +10,14 @@ public class playerController : MonoBehaviour {
     public InputField tf_X;
     public InputField tf_Y;
     public InputField tf_Z;
+    public bool isClick;
 
     [SerializeField]
     Vector3 vetocity = Vector3.zero;
     [SerializeField]
     float accelemation = 4f;
-    //[SerializeField]
-    //float minVelocity;
+    [SerializeField]
+    float minVelocity;
     [SerializeField]
     float maxVelocity = 100;
     Camera cam;
@@ -27,9 +28,7 @@ public class playerController : MonoBehaviour {
         //Button btn1 = runBtn.GetComponent<Button>();
         //runBtn.onClick.AddListener(TaskOnClick);
 
-        textX.text = "X:" + "1";
-        textY.text = "X:" + "11";
-        textZ.text = "X:" + "12";
+
     }
    
     //private void Awake()
@@ -42,73 +41,135 @@ public class playerController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        if (isClick == true)
+        {
+            print("vo");
+            //move forward
+            if (Input.GetKey(KeyCode.W))
+            {
+                vetocity -= transform.forward * accelemation * Time.deltaTime;//new Vector2(0, 1 * Time.deltaTime);
+            }
 
-        //move forward
-        if (Input.GetKey(KeyCode.W))
-        {
-            vetocity += transform.forward * accelemation * Time.deltaTime;//new Vector2(0, 1 * Time.deltaTime);
-        }
+            //rotate left
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(new Vector3(0, 5 * accelemation * Time.deltaTime, 0));
+            }
+            //rotate right
+            if (Input.GetKey(KeyCode.X))
+            {
+                transform.Rotate(new Vector3(0, -5 * accelemation * Time.deltaTime, 0));
+            }
 
-        //rotate left
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(new Vector3(0, -5 * 0.75f, 0));
-        }
-        //rotate right
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(new Vector3(0, 5 * 0.75f, 0));
-        }
-        //clamp velocity
-        //vetocity = new Vector3(
-        //Mathf.Clamp(vetocity.x, minVelocity, maxVelocity),
-        //Mathf.Clamp(vetocity.x, minVelocity, maxVelocity),
-        //0);
-        vetocity = Vector3.ClampMagnitude(vetocity, maxVelocity);
-        print(vetocity.x);
 
-        transform.position += (vetocity * 0.75f);
+            //clamp velocity
+            vetocity = new Vector3(
+            Mathf.Clamp(vetocity.x, minVelocity, maxVelocity),
+            Mathf.Clamp(-vetocity.x, minVelocity, maxVelocity),
+            0);
+            //vetocity = Vector3.ClampMagnitude(vetocity, maxVelocity);
+            print(vetocity.x);
+            print(vetocity.y);
+            print(vetocity.z);
 
-        //check if we'ar off screen and resolve
-        //Vector3 screenPos = cam.WorldToViewportPoint(transform.position);
-        Vector3 screenPos = cam.WorldToViewportPoint(transform.position);
-        // set y
-        if (screenPos.y < 0)
-        {
-            screenPos.y = 1;
-        }
-        else if (screenPos.y > 1)
-        {
-            screenPos.y = 0;
-        }
-        //set x
-        if (screenPos.x < 0)
-        {
-            screenPos.x = 1;
-        }
-        else if (screenPos.x > 1)
-        {
-            screenPos.x = 0;
-        }
-        //textX.text = "X:" + "1";
-        //textY.text = "X:" + "11";
-        //textZ.text = "X:" + "12";
+            transform.position -= (vetocity * -0.75f);
+       
+            //check if we'ar off screen and resolve
+            //Vector3 screenPos = cam.WorldToViewportPoint(transform.position);
+            Vector3 screenPos = cam.WorldToViewportPoint(transform.position);
+            // set y
+            if (screenPos.y < 0)
+            {
+                screenPos.y = 1;
+            }
+            else if (screenPos.y > 1)
+            {
+                screenPos.y = 0;
+            }
+            //set x
+            if (screenPos.x < 0)
+            {
+                screenPos.x = 1;
+            }
+            else if (screenPos.x > 1)
+            {
+                screenPos.x = 0;
+            }
 
-        transform.position = cam.ViewportToWorldPoint(screenPos);
+
+            transform.position = cam.ViewportToWorldPoint(screenPos);
+        }
+        else
+        {
+            //move forward
+            if (Input.GetKey(KeyCode.W))
+            {
+                vetocity += transform.forward * accelemation * Time.deltaTime;//new Vector2(0, 1 * Time.deltaTime);
+            }
+
+            //rotate left
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(new Vector3(0, -5 * accelemation * Time.deltaTime, 0));
+            }
+            //rotate right
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(new Vector3(0, 5 * 0.75f, 0));
+            }
+
+
+            //clamp velocity
+            vetocity = new Vector3(
+            Mathf.Clamp(vetocity.x, minVelocity, maxVelocity),
+            Mathf.Clamp(vetocity.x, minVelocity, maxVelocity),
+            0);
+            //vetocity = Vector3.ClampMagnitude(vetocity, maxVelocity);
+            //print(vetocity.x);
+
+            transform.position += (vetocity * accelemation * Time.deltaTime);
+
+            //check if we'ar off screen and resolve
+            //Vector3 screenPos = cam.WorldToViewportPoint(transform.position);
+            Vector3 screenPos = cam.WorldToViewportPoint(transform.position);
+            // set y
+            if (screenPos.y < 0)
+            {
+                screenPos.y = 1;
+            }
+            else if (screenPos.y > 1)
+            {
+                screenPos.y = 0;
+            }
+            //set x
+            if (screenPos.x < 0)
+            {
+                screenPos.x = 1;
+            }
+            else if (screenPos.x > 1)
+            {
+                screenPos.x = 0;
+            }
+
+
+            transform.position = cam.ViewportToWorldPoint(screenPos);
+        }
+            //print(transform.position);
+        textX.text = "" + transform.position.x;
+        textY.text = "" + transform.position.y;
+        textZ.text = "" + transform.position.z;
         //next
     }
 
     public void TaskOnClick()
     {
+        isClick = true;
         //Output this to console when the Button is clicked
         Debug.Log("You have clicked the button!");
-        textX.text = "X:" + "111111";
-        textY.text = "X:" + "11111";
-        textZ.text = "X:" + "1112";
-
-        tf_X.text = "X:" + "111111";
-        tf_Y.text = "X:" + "11111";
-        tf_Z.text = "X:" + "1112";
+  
+        tf_X.text = "" + transform.position.x;
+        tf_Y.text = "" + transform.position.y;
+        tf_Z.text = "" + transform.position.z;
     }
 
 
